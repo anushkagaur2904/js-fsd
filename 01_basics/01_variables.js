cf5fae595752f61df0d48069a0337d3e0536f50e
Anushka Gaur <anushkagaur52@gmail.com>
2026-02-06 06:21:37 +0000

    Basics done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+//const mtlb value change nhi kr skte
+
+const accountId = 123
+let accountEmail = "anushka@gmail.com"
+var accountPass = "123"
+accountCity = "Jaipur"
+let accountState
+
+//accountId=2 // not allowed
+accountEmail = "hchc@.com"
+accountPass = "2121"
+accountCity = "Bengaluru"
+
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
+
+//difference between let and var??
+
+//accountCity ke aage na let likha na var likha (not recommended tho)
+
+//{}-> curly braces scope batate hai
+
+/*prefer not to use var cuz of issue in block scope and functional scope */
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";// code will be treated as newer version of js, ; lagana idhr
+
+// alert("hello")// will give error in terminal cuz we are using nodejs not browser
+
+// js ka engine browser ke andar chupa hai
+
+// code readability should be high
+// tc39 and mdn docs
+/*
+primitive data types =>
+    number,bigint,string,boolean,null(standalone value),undefined(value abhi assigned nhi),symbol(unique)
+*/
+//object is also datatype
+let state = null// mtlb ki empty
+
+console.log(typeof null)//null is an object (bug hai cuz null toh datatype hai)
+
+console.log(typeof undefined);//undefined
